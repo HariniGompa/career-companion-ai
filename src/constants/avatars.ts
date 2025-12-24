80304717a90d53b6ad8020079e68761dffc68500
Harini Gompa <harinigompa999@gmail.com>
2025-12-24 13:48:42 +0530

    Add files via upload

---
+++ src/constants/avatars.ts
@@ -0,0 +1,14 @@
+// src/constants/avatars.ts
+
+export const AVATARS = [
+  {
+    id: "avatar_female_1",
+    label: "Female Avatar",
+    src: "/avatars/female-1.png",
+  },
+  {
+    id: "avatar_male_1",
+    label: "Male Avatar",
+    src: "/avatars/male-1.png",
+  },
+];
