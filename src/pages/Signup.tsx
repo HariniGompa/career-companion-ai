88e23dbe3b3d650baa369d3add78a99378706230
Harini Gompa <harinigompa999@gmail.com>
2025-12-25 11:06:01 +0530

    Update Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -25,7 +25,6 @@ const Signup = () => {
   const [privacyOpen, setPrivacyOpen] = useState(false);
 
   const passwordsMatch = password === confirmPassword && confirmPassword.length > 0;
-  const passwordValid = password.length >= 8;
 
   // Redirect if already logged in
   useEffect(() => {
